58b366d7f939ed94a665cccc9bfa9658ff887ccb
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-09-27 15:08:32 -0300

    Requisito 3

--- src/challenges.js
+++ src/challenges.js
@@ -13,8 +13,8 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(myString) {
+  return myString.split(" ");
 }
 
 // Desafio 4
