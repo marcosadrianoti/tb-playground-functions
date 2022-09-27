3488e7a2786dbaf9336725c70d644b029fc9a78f
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-09-27 15:17:37 -0300

    Requisito 5

--- src/challenges.js
+++ src/challenges.js
@@ -23,8 +23,8 @@ function concatName(myArray) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return (wins * 3) + ties;
 }
 
 // Desafio 6
